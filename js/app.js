967bda20209aebd70e647f628ee54e428cceecf5
Andrew Nguyen <andrew.nguyen@kwallcompany.com>
2019-04-11 18:56:03 -0700

    updates

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -27,7 +27,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -40,14 +40,14 @@
   $(window).scroll(navbarCollapse);
 
   // Floating label headings for the contact form
-  $(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-      $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-      $(this).removeClass("floating-label-form-group-with-focus");
-    });
-  });
+  // $(function() {
+  //   $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+  //     $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+  //   }).on("focus", ".floating-label-form-group", function() {
+  //     $(this).addClass("floating-label-form-group-with-focus");
+  //   }).on("blur", ".floating-label-form-group", function() {
+  //     $(this).removeClass("floating-label-form-group-with-focus");
+  //   });
+  // });
 
 })(jQuery); // End of use strict
